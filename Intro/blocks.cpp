858b34ea87ef752e8042283c781776f3b573194d
Seth Ullman <seth.ullman98@gmail.com>
2020-01-17 16:30:35 -0700

    add block letters project

---
+++ Intro/blocks.cpp
@@ -0,0 +1,16 @@
+//My name is Seth Ullman and I'm a software //engineer
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "SSSSSSSSSS     U         U\n";
+  std::cout << "S              U         U\n";
+  std::cout << "S              U         U\n";
+  std::cout << "S              U         U\n";
+  std::cout << "SSSSSSSSSS     U         U\n";
+  std::cout << "         S     U         U\n";
+  std::cout << "         S     U         U\n";
+  std::cout << "         S      U       U \n";
+  std::cout << "SSSSSSSSSS       UUUUUUU  \n";
+}
